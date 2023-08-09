96a233ef866c95099e854b570ab9bcd08c2dde93
meliksahc <mlkshcmgz@outlook.com>
2023-08-09 19:05:41 +0300

    finish

---
+++ MelrinRescue/Assets/Scripts/time.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class time : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void oyunubaslat()
+    {
+        Time.timeScale = 1;
+    }
+}
